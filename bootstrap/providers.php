37e22d2ed8b54af3a24eecc1fabdfc7818bab2ff
DiveshR <divesh20july@gmail.com>
2026-07-03 04:40:59 +0530

    api setup and testing

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\PatternCheckoutServiceProvider;
 
 return [
     AppServiceProvider::class,
+    PatternCheckoutServiceProvider::class,
+
+
 ];
